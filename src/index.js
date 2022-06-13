99d638c5660358a0bb9aa644fcc30f75f8a4fa2f
MatheuSS <46387641+MatheusHenriqueSS@users.noreply.github.com>
2022-06-12 22:07:02 -0300

    first jsx practice

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>List of things a hate most</h1>
+    <ul>
+      <li>Coding</li>
+      <li>Lists</li>
+      <li>Irony</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
