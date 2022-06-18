d4799abe9fa32a18caed8fd6997f9bd8152a0be2
MatheuSS <matheushss2013@gmail.com>
2022-06-18 01:09:53 -0300

    add props to card

--- react-props/src/Card.jsx
+++ react-props/src/Card.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 
-function Card() {
+function Card(props) {
     return (
         <div>
-            <h2>Beyonce</h2>
-            <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-            />
-            <p>+123 456 789</p>
-            <p>b@beyonce.com</p>
+            <h2>{props.name}</h2>
+            <img src={props.img}/>
+            <p>{props.tel}</p>
+            <p>{props.mail}</p>
         </div>
     );
 }
+
+export default Card;
