514c9aba3695d9f42cfab54dfbae06ef43a30c12
MatheuSS <matheushss2013@gmail.com>
2022-06-18 22:39:23 -0300

    renders note.js using map

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./Header"
 import Footer from "./Footer";
-import Note from "./Note"
+import Note from "./Note";
+import notes from "../notes";
 
 function App() {
-    return (<div>
-    <Header/>
-    <Note/>
-    <Footer/>
+    return (
+    <div>
+        <Header/>
+        {notes.map(p => <Note key={p.id} title={p.title} content={p.content}/>)}
+        <Footer/>
     </div>);
 }
 
